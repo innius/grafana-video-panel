6e4c247807a9c200bfe74d326046bebb66ac1156
Loredana Popescu <loredana@falcontrading.ro>
2020-09-15 14:06:26 +0300

    Updates

--- src/VideoPanel.test.tsx
+++ src/VideoPanel.test.tsx
@@ -3,36 +3,34 @@ import Enzyme, { shallow } from 'enzyme';
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('VideoPanel', () => {
   it('renders a video field when the selected type is video url', () => {
     const component = mockComponent({
       videoType: 'url',
       autoPlay: false,
-      loop: false
-    })
+      loop: false,
+    });
     expect(component.find('video')).toHaveLength(1);
   });
-
 
   it('renders an iframe field when the selected type is youtube id', () => {
     const component = mockComponent({
       videoType: 'youtube',
       autoPlay: false,
       loop: false,
-    })
+    });
 
     expect(component.find('iframe')).toHaveLength(1);
   });
-
 
   it('renders an iframe field when the selected type is ifrane', () => {
     const component = mockComponent({
       videoType: 'iframe',
       autoPlay: true,
       loop: false,
-    })
+    });
 
     expect(component.find('iframe')).toHaveLength(1);
   });
@@ -43,11 +41,11 @@ describe('VideoPanel', () => {
       videoId: '123',
       autoPlay: true,
       loop: true,
-    })
+    });
 
-    const iframeSrc = component.find('iframe').props().src
-    expect(iframeSrc).toContain('autoplay=1&loop=1')
-    expect(iframeSrc).toContain('embed/123')
+    const iframeSrc = component.find('iframe').props().src;
+    expect(iframeSrc).toContain('autoplay=1&loop=1');
+    expect(iframeSrc).toContain('embed/123');
   });
 
   it('renders a video with autoplay and loop true', () => {
@@ -55,16 +53,16 @@ describe('VideoPanel', () => {
       videoType: 'url',
       autoPlay: true,
       loop: true,
-    })
-    const videoControls = component.find('video').props()
-    expect(videoControls.loop).toBeTruthy()
-    expect(videoControls.autoPlay).toBeTruthy()
+    });
+    const videoControls = component.find('video').props();
+    expect(videoControls.loop).toBeTruthy();
+    expect(videoControls.autoPlay).toBeTruthy();
   });
 
   function mockComponent(options: any) {
     const defaultProps: any = {
-      options: options
-    }
-    return shallow(<VideoPanel {...defaultProps} />)
+      options: options,
+    };
+    return shallow(<VideoPanel {...defaultProps} />);
   }
 });
